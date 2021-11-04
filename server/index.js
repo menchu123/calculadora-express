9e0ac6010c48ee8608da82ec476bf43f09d0a4fa
Menchu MV <mcmvillalobos@gmail.com>
2021-11-04 13:41:19 +0100

    Added error message if server fails

--- server/index.js
+++ server/index.js
@@ -9,6 +9,13 @@ const initializeServer = (port) => {
   const server = app.listen(port, () => {
     debug(chalk.yellow(`Escuchando en el puerto ${port}`));
   });
+
+  server.on("error", (error) => {
+    debug(chalk.red("Ha habido un error al iniciar el servidor."));
+    if (error.code === "EADDRINUSE") {
+      debug(chalk.red(`El puerto ${port} está en uso.`));
+    }
+  });
 };
 
 module.exports = initializeServer;
